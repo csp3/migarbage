871ba4f41d0c9ff6db37cc6f9a16271c3c80931a
krlos solis <solis.krlos@gmail.com>
2019-10-22 17:12:26 -0500

    tc

--- MemoryLeakDetector-master/mld.h
+++ MemoryLeakDetector-master/mld.h
@@ -1,7 +1,7 @@
 /*
  * =====================================================================================
  *
- *       Filename:  mld.h
+ *       Filename:  mld.h 
  *
  *    Description:  This file defines the data structures used for MLD tool
  *
@@ -40,72 +40,68 @@
 #define MAX_FIELD_NAME_SIZE 128
 
 /*Enumeration for data types*/
-typedef enum {
-    UINT8,
-    UINT32,
-    INT32,
-    CHAR,
-    OBJ_PTR,
-    VOID_PTR, /*New Data type added to identify void * pointers*/
-    FLOAT,
-    DOUBLE,
-    OBJ_STRUCT
+typedef enum
+{
+	UINT8,
+	UINT32,
+	INT32,
+	CHAR,
+	OBJ_PTR,
+	VOID_PTR, /*New Data type added to identify void * pointers*/
+	FLOAT,
+	DOUBLE,
+	OBJ_STRUCT
 } data_type_t;
 
-typedef enum{
-
-    MLD_FALSE,
-    MLD_TRUE
+typedef enum
+{
+	MLD_FALSE,
+	MLD_TRUE
 } mld_boolean_t;
 
+#define OFFSETOF(struct_name, fld_name) (unsigned int)&(((struct_name *)0)->fld_name)
 
-
-#define OFFSETOF(struct_name, fld_name)     \
-    (unsigned int)&(((struct_name *)0)->fld_name)
-
-#define FIELD_SIZE(struct_name, fld_name)   \
-    sizeof(((struct_name *)0)->fld_name)
-
+#define FIELD_SIZE(struct_name, fld_name) sizeof(((struct_name *)0)->fld_name)
 
 typedef struct _struct_db_rec_ struct_db_rec_t;
 
 /*Structure to store the information of one field of a 
  * C structure*/
-typedef struct _field_info_{
-    char fname [MAX_FIELD_NAME_SIZE];   /*Name of the field*/
-    data_type_t dtype;                  /*Data type of the field*/
-    unsigned int size;                  /*Size of the field*/
-    unsigned int offset;                /*Offset of the field*/
-    // Below field is meaningful only if dtype = OBJ_PTR, Or OBJ_STRUCT
-    char nested_str_name[MAX_STRUCTURE_NAME_SIZE];
+typedef struct _field_info_
+{
+	char fname[MAX_FIELD_NAME_SIZE]; /*Name of the field*/
+	data_type_t dtype;					/*Data type of the field*/
+	unsigned int size;					/*Size of the field*/
+	unsigned int offset;					/*Offset of the field*/
+	// Below field is meaningful only if dtype = OBJ_PTR, Or OBJ_STRUCT
+	char nested_str_name[MAX_STRUCTURE_NAME_SIZE];
 } field_info_t;
 
 /*Structure to store the information of one C structure
  * which could have 'n_fields' fields*/
-struct _struct_db_rec_{
-    struct_db_rec_t *next;  /*Pointer to the next structure in the linked list*/
-    char struct_name [MAX_STRUCTURE_NAME_SIZE];  // key
-    unsigned int ds_size;   /*Size of the structure*/
-    unsigned int n_fields;  /*No of fields in the structure*/
-    field_info_t *fields;   /*pointer to the array of fields*/
+struct _struct_db_rec_
+{
+	struct_db_rec_t *next;							 /*Pointer to the next structure in the linked list*/
+	char struct_name[MAX_STRUCTURE_NAME_SIZE]; // key
+	unsigned int ds_size;							 /*Size of the structure*/
+	unsigned int n_fields;							 /*No of fields in the structure*/
+	field_info_t *fields;							 /*pointer to the array of fields*/
 };
 
 /*Finally the head of the linked list representing the structure
  * database*/
-typedef struct _struct_db_{
-    struct_db_rec_t *head;
-    unsigned int count;
+typedef struct _struct_db_
+{
+	struct_db_rec_t *head;
+	unsigned int count;
 } struct_db_t;
-
 
 /*Structure Data base Definition Ends*/
 
 /* Printing functions*/
-void
-print_structure_rec (struct_db_rec_t *struct_rec);
+void print_structure_rec(struct_db_rec_t *struct_rec);
 
-void
-print_structure_db(struct_db_t *struct_db);
+void print_structure_db(struct_db_t *struct_db);
 
 /* Fn to add the structure record in a structure database */
 
@@ -114,80 +110,69 @@ add_structure_to_struct_db(struct_db_t *struct_db, struct_db_rec_t *struct_rec);
 
 /*Structure Registration helping APIs*/
 
-#define FIELD_INFO(struct_name, fld_name, dtype, nested_struct_name)    \
-   {#fld_name, dtype, FIELD_SIZE(struct_name, fld_name),                \
-        OFFSETOF(struct_name, fld_name), #nested_struct_name} 
+#define FIELD_INFO(struct_name, fld_name, dtype, nested_struct_name) \
+	{                                                                 \
+#fld_name, dtype, FIELD_SIZE(struct_name, fld_name),           \
+			 OFFSETOF(struct_name, fld_name), #nested_struct_name       \
+	}
 
-#define REG_STRUCT(struct_db, st_name, fields_arr)                    \
-    do{                                                               \
-        struct_db_rec_t *rec = calloc(1, sizeof(struct_db_rec_t));    \
-        strncpy(rec->struct_name, #st_name, MAX_STRUCTURE_NAME_SIZE); \
-        rec->ds_size = sizeof(st_name);                              \
-        rec->n_fields = sizeof(fields_arr)/sizeof(field_info_t);     \
-        rec->fields = fields_arr;                                    \
-        if(add_structure_to_struct_db(struct_db, rec)){              \
-            assert(0);                                               \
-        }                                                            \
-    }while(0);
+#define REG_STRUCT(struct_db, st_name, fields_arr)                  \
+	do                                                               \
+	{                                                                \
+		struct_db_rec_t *rec = calloc(1, sizeof(struct_db_rec_t));    \
+		strncpy(rec->struct_name, #st_name, MAX_STRUCTURE_NAME_SIZE); \
+		rec->ds_size = sizeof(st_name);                               \
+		rec->n_fields = sizeof(fields_arr) / sizeof(field_info_t);    \
+		rec->fields = fields_arr;                                     \
+		if (add_structure_to_struct_db(struct_db, rec))               \
+		{                                                             \
+			assert(0);                                                 \
+		}                                                             \
+	} while (0);
 
 /*Structure Data base Definition Ends*/
-
-
-
-
-
-
-
-
-
-
 
 /*Object Database structure definitions Starts here*/
 
 typedef struct _object_db_rec_ object_db_rec_t;
 
-struct _object_db_rec_{
-    object_db_rec_t *next;
-    void *ptr;
-    unsigned int units;
-    struct_db_rec_t *struct_rec;
-    mld_boolean_t is_visited; /*Used for Graph traversal*/
-    mld_boolean_t is_root;    /*Is this object is Root object*/
+struct _object_db_rec_
+{
+	object_db_rec_t *next;
+	void *ptr;
+	unsigned int units;
+	struct_db_rec_t *struct_rec;
+	mld_boolean_t is_visited; /*Used for Graph traversal*/
+	mld_boolean_t is_root;	 /*Is this object is Root object*/
 };
 
-typedef struct _object_db_{
-    struct_db_t *struct_db;
-    object_db_rec_t *head;
-    unsigned int count;
+typedef struct _object_db_
+{
+	struct_db_t *struct_db;
+	object_db_rec_t *head;
+	unsigned int count;
 } object_db_t;
 
-
 /*Dumping functions*/
-void
-print_object_rec(object_db_rec_t *obj_rec, int i);
+void print_object_rec(object_db_rec_t *obj_rec, int i);
 
-void
-print_object_db(object_db_t *object_db);
+void print_object_db(object_db_t *object_db);
 
 /*API to malloc the object*/
-void*
+void *
 xcalloc(object_db_t *object_db, char *struct_name, int units);
 
 /*APIs to register root objects*/
-void mld_register_root_object (object_db_t *object_db, 
-                               void *objptr, 
-                               char *struct_name, 
-                               unsigned int units);
+void mld_register_root_object(object_db_t *object_db,
+										void *objptr,
+										char *struct_name,
+										unsigned int units);
 
-void
-set_mld_object_as_global_root(object_db_t *object_db, void *obj_ptr);
-
+void set_mld_object_as_global_root(object_db_t *object_db, void *obj_ptr);
 
 /*APIs for MLD Algorithm*/
-void
-run_mld_algorithm(object_db_t *object_db);
+void run_mld_algorithm(object_db_t *object_db);
 
-void
-report_leaked_objects(object_db_t *object_db);
+void report_leaked_objects(object_db_t *object_db);
 
 #endif /* __MLD__ */
---
+++ mld.h
@@ -0,0 +1,80 @@
+/*Nombre de archivo: mld.h 
+Descripción:  Este fichero define las estructuras de datos utilizadas para la herramienta MLD
+Versión: 1.0 
+Creado:  Jueves 28 de febrero de 2019 05:14:18 IST
+Revisión: 1.0
+Compilador: gcc
+ 
+Autor:  Er. Abhishek Sagar, Desarrollador de redes (AS), sachinites@gmail.com
+Compañía:  Brocade Communications(Jul 2012- Mar 2016), Actual : Juniper Networks(Abr 2017 - Presente)
+
+Copyright (c) 2017 Abhishek Sagar.
+Este programa es software libre: puede redistribuirlo y/o modificarlo. 
+*/
+
+#ifndef __MLD__
+#include <assert.h>
+#include <string.h>
+
+#define MAX_STRUCTURE_NAME_SIZE 128
+#define MAX_FIELD_NAME_SIZE 128
+
+typedef enum
+{
+	UINT8,
+	UINT32,
+	INT32,
+	CHAR,
+	OBJ_PTR,
+	VOID_PTR, /para identificar void * pointers*/
+	FLOAT,
+	DOUBLE,
+	OBJ_STRUCT
+} data_type_t;
+
+typedef enum
+{
+	MLD_FALSE,
+	MLD_TRU
+} mld_boolean_t;
+
+#define OFFSETOF(struct_name, fld_name) (unsigned int)&(((struct_name *)0)->fld_name)
+#define FIELD_SIZE(struct_name, fld_name) sizeof(((struct_name *)0)->fld_name)
+
+typedef struct _struct_db_rec_ struct_db_rec_t;
+
+/*estructura que almacena información de los campos*/
+typedef struct _field_info_
+{
+	char fname[MAX_FIELD_NAME_SIZE]; /*nombre del campo*/
+	data_type_t dtype; /*tipo del campo*/
+	unsigned int size; /*tamaño del campo*/
+	unsigned int offset; /*offset del campo*/
+	// El campo de abajo sólo tiene sentido si dtype = OBJ_PTR o OBJ_STRUCT
+	char nested_str_name[MAX_STRUCTURE_NAME_SIZE];
+} field_info_t;
+
+/*estructura para almacenar la información de una estructura C que podría tener N campos*/
+struct _struct_db_rec_
+{
+	struct_db_rec_t *next; /*puntero a la siguiente estructura de la lista enlazada*/
+	char struct_name[MAX_STRUCTURE_NAME_SIZE]; //key
+	unsigned int ds_size; /*tamaño de la estructura*/
+	unsigned int n_fields: /*numero de campos*/
+	field_info_t *field_info_t; /*puntero al array de campos de esa estructura*/
+};
+
+/*la cabecera de la lista enlazada que representa la base de datos de la estructura*/
+typedef struct _struct_db_
+{
+	struct_db_rec_t *head;
+	unsigned int count;
+} struct_db_t;
+
+/*definición de la base de datos de estructurad finales*/ 
+
+/*funciones de impresión*/
+void print_structure_rec(struct_db_rec_t *struct_rec);
+void print_structure_db(struct_db_t *struct_db);
+
+#endif /* __MLD__ */
